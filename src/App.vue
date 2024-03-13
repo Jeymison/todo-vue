e684c3374fe92a6f442fc106bc479a99f158fe11
Jeymison Menezes <jeymisong@gmail.com>
2024-03-12 22:22:13 -0300

    Criando todo-list em um arquivo

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,102 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+const estado = reactive({
+  tarefaTemporaria: '',
+
+  filtro:'todas',
+
+  tarefas: [
+  {
+    titulo: 'Estudar ES6',
+    finalizada: false,
+  },
+  {
+    titulo: 'Estudar SASS',
+    finalizada: false,
+  },
+  {
+    titulo: 'Ir malhar',
+    finalizada: true,
+  }
+  ]
+})
+
+const getTarefasPendentes = () => {
+  return estado.tarefas.filter(tarefa => !tarefa.finalizada) // falsa
+}
+
+const getTarefasFinalizadas = () => {
+  return estado.tarefas.filter(tarefa => tarefa.finalizada) // verdadeiro
+}
+
+const getTarefasFiltradas = () => {
+  const {filtro} = estado;
+
+  switch(filtro) {
+    case 'pendentes':
+      return getTarefasPendentes();
+    case 'finalizadas':
+      return getTarefasFinalizadas();
+    default:
+      return estado.tarefas;
+    
+  }
+}
+
+const cadastraTarefa = () => {
+  const tarefaNova = {
+    titulo: estado.tarefaTemporaria,
+    finalizada: false,
+  }
+  estado.tarefas.push(tarefaNova);
+  estado.tarefaTemporaria = '';
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+<div class="container">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+    <h1>Minhas Tarefas</h1>
+    <p>
+      Voce possui {{ getTarefasPendentes().length }} tarefas pendentes
+    </p>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- // para atualizar o cadastro com o novo item usamos .prevent -->
+  <form @submit.prevent="cadastraTarefa"> 
+  <div class="row">
+    <div class="col">
+      <input :value="estado.tarefaTemporaria" @change="evento => estado.tarefaTemporaria = evento.target.value" required type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+    </div>
+    <div class="col-md-1">
+      <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </div>
+    <div class="col-md-2">
+      <select @change="evento => estado.filtro = evento.target.value" class="form-control">
+        <option value="todas">Todas as tarefas</option>
+        <option value="pendentes">Pendentes</option>
+        <option value="finalizadas">Finalizadas</option>
+      </select>
+    </div>
+  </div>
+  </form>
+
+  <ul class="list-group mt-4">
+    <li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
+      <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
+      <label :class="{done: tarefa.finalizada == true}" class="ms-3" :for="tarefa.titulo">
+        {{ tarefa.titulo }}
+      </label>
+    </li>
+  </ul>
+
+</div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.done {
+  text-decoration: line-through;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
+//importanto o css do BootStrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).mount('#app')
